ddd9d041e6eee2019c62ade3fe8773d936943741
Natalia Comrakova <cnatalia7472@Natalias-iMac.local>
2018-10-23 02:23:21 +1000

    mobilemenu

--- src/components/widget/Navigation.js
+++ src/components/widget/Navigation.js
@@ -4,12 +4,12 @@ import {
     Link    
 } from 'react-router-dom';
 
-import {isMobile} from 'react-device-detect';
+// import {isMobile} from 'react-device-detect';
 
 class Navigation extends React.Component {
    
     render() {
-        if (!isMobile) {
+        // if (!isMobile) {
             return (
                 <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
                     <div className = "container">
@@ -66,50 +66,50 @@ class Navigation extends React.Component {
                     </div>
                 </nav>
             );
-        } else {
-            return (
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
-                    <div className = "container">
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+        // } else {
+        //     return (
+        //         <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
+        //             <div className = "container">
+        //                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //                     <span className="navbar-toggler-icon"></span>
+        //                 </button>
 
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <Link className="nav-link" to="/news">News <span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/features">Features</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/media">Media</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/team">Team</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/contact">Contact</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <ul className="navbar-nav navbar-right">
-                            <li className="nav-item">
-                                <Link className = "nav-link shoping-card" to = "/help">
-                                    <img alt="..." src = {assets.shopping_cart_black}/>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className = "nav-link message-group" to = "/help">
-                                    <img alt="..." src = {assets.user}/>
-                                    <div className = "badge">2</div>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            );
-        }
+        //                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        //                     <ul className="navbar-nav mr-auto">
+        //                         <li className="nav-item active">
+        //                             <Link className="nav-link" to="/news">News <span className="sr-only">(current)</span></Link>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <Link className="nav-link" to="/features">Features</Link>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <Link className="nav-link" to="/media">Media</Link>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <Link className="nav-link" to="/team">Team</Link>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <Link className="nav-link" to="/contact">Contact</Link>
+        //                         </li>
+        //                     </ul>
+        //                 </div>
+        //                 <ul className="navbar-nav navbar-right">
+        //                     <li className="nav-item">
+        //                         <Link className = "nav-link shoping-card" to = "/help">
+        //                             <img alt="..." src = {assets.shopping_cart_black}/>
+        //                         </Link>
+        //                     </li>
+        //                     <li className="nav-item">
+        //                         <Link className = "nav-link message-group" to = "/help">
+        //                             <img alt="..." src = {assets.user}/>
+        //                             <div className = "badge">2</div>
+        //                         </Link>
+        //                     </li>
+        //                 </ul>
+        //             </div>
+        //         </nav>
+        //     );
+        // }
     }
 }
 
